24278daf5405ae23e1f2377af90a293a055da134
Afroj Mulani <mulaniafroj26@gmail.com>
2025-09-14 02:39:03 +0530

    update

---
+++ frontend/src/assets/ProductAssets.jsx
@@ -0,0 +1,92 @@
+export const products = [
+  {
+    title: "H&E Pencils",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757089376/uploads/wmff1u6acg4urmonm5xf.jpg"
+  },
+  {
+    title: "Knife",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608718/uploads/ioblvhqr0qhsufnfo4ze.jpg"
+  },
+  {
+    title: "Enamel Tray",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757337273/uploads/b0dcs4bg2qixw6dvz2ai.jpg"
+  },
+  {
+    title: "Bond papers",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608295/uploads/khobrwisyfv7tuoy0sii.webp"
+  },
+  {
+    title: "Wax",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608467/uploads/i68xdwkhcyixgvwjclgf.webp"
+  },
+  {
+    title: "Straight and Curved Spatula",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608571/uploads/mheyzo6ljhkqxfqhwjdc.webp"
+  },
+  {
+    title: "Spacer Wax",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608617/uploads/uv8tlbzkxjpy6gesf4yb.webp"
+  },
+  {
+    title: "Cotton Gauze",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608667/uploads/aek77lclojoemizthrzw.webp"
+  },
+  {
+    title: "Gloves",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608837/uploads/fhy8hl6jz26zejbdindy.jpg"
+  },
+  {
+    title: "Rubber Bowls",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608876/uploads/arg4x9a56dqsfwutvafh.jpg"
+  },
+  {
+    title: "Rubber Bowl with S&C Spatula",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608911/uploads/f6v0b4ylk80bgg5ziwgs.jpg"
+  },
+  {
+    title: "Mackintosh Sheet",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757608987/uploads/dxd5jdzpejx1rfrnjzyd.jpg"
+  },
+  {
+    title: "Modeling Wax",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757609041/uploads/qbzsnejpjkkahhd1ndcs.jpg"
+  },
+  {
+    title: "Wax Knife",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757609087/uploads/kxawle2ldq9vw8mzgo57.jpg"
+  },
+  {
+    title: "Hot Plate",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757609180/uploads/qcefdbrkhlnmjbbqgvlb.jpg"
+  },
+  {
+    title: "Bond Papers",
+    imageUrl: "https://res.cloudinary.com/dlsobs4zm/image/upload/v1757609222/uploads/xhx58zojerth90tjpkii.jpg"
+  },
+  {
+    title: "Carver",
+    imageUrl: "https://images1.dentalkart.com/media/catalog/product/l/e/lecron_s_carver_1_.jpg"
+  },
+  {
+    title: "Stainless Steel Ruler",
+    imageUrl: "https://m.media-amazon.com/images/I/8174ACNgzRL._AC_SL1500_.jpg"
+  },
+  {
+    title: "Dental Glass Slabs",
+    imageUrl: "https://image.made-in-china.com/2f0j00GpkcQdahgnbP/Polished-Glass-Dental-Impression-Materials-Mixing-Slabs-with-Beveled-Edges.jpg"
+  },
+  {
+    title: "Enamel Trays",
+    imageUrl: "https://tse1.mm.bing.net/th/id/OIP.j1YGSyGSNUQqzRbj4ysZuAHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
+  },
+  {
+    title: "Face Mask",
+    imageUrl: "https://cdn.shopify.com/s/files/1/2399/4151/products/motorwerks_masks_1600x.jpg?v=1585660596"
+  },
+  {
+    title: "Head Cover",
+    imageUrl: "https://gcaremedics.com/cdn/shop/files/Blue-Bouffant-Caps-21-Round-Disposable-Head-Covers.webp?v=1734188701&width=1946"
+  }
+
+
+];
